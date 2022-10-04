d9636cd35f28db5cc34d7268159e1c402c8c1fb7
Teodora Malevinskaya <amalevinskaya@ozon.ru>
2022-10-04 04:21:00 +0300

    hmm

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"gitlab.ozon.dev/amalevinskaya/teodora-malevinskaia/internal/clients/tg"
+	"gitlab.ozon.dev/amalevinskaya/teodora-malevinskaia/internal/config"
+	"gitlab.ozon.dev/amalevinskaya/teodora-malevinskaia/internal/model/messages"
+	"log"
+)
+
+func main() {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
